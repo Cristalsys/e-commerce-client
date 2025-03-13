a66c2f8e7a0401b530f2e9c442738c7e55e021e3
Cristalsys <cristals22332233n@gmail.com>
2025-03-13 18:54:46 +0200

    second commit 17

--- src/server-actions/middlewares/utils/get-tokens-from-request.ts
+++ src/server-actions/middlewares/utils/get-tokens-from-request.ts
@@ -7,14 +7,19 @@ export async function getTokensFromRequest(request: NextRequest) {
 	const refreshToken = request.cookies.get(EnumTokens.REFRESH_TOKEN)?.value
 	let accessToken = request.cookies.get(EnumTokens.ACCESS_TOKEN)?.value
 
+	console.log('Все куки:', request.cookies.getAll())
+	console.log('refreshToken:', refreshToken)
+
 	if (!refreshToken) {
 		request.cookies.delete(EnumTokens.ACCESS_TOKEN)
 		return null
 	}
 
+	console.log('Отправляем refreshToken:', refreshToken)
 	if (!accessToken) {
 		try {
 			const data = await getNewTokensByRefresh(refreshToken)
+			console.log('Получены новые токены:', data)
 			accessToken = data.accessToken
 		} catch (error) {
 			if (error instanceof Error) {
